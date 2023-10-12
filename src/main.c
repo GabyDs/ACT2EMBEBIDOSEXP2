4ee202a55fae6388916bf5526e466b3d961c3971
Gabriel Da Silva <gabrields@pop-os.localdomain>
2023-10-12 15:44:14 -0300

    config timer to fast pwm mode

--- src/main.c
+++ src/main.c
@@ -1,5 +1,28 @@
-#include <avr.io>;
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
 
 int main(void)
 {
+    // Config compare TOP and BOTTOM to width pulse
+    OCR2A = 125;
+    OCR2B = 50;
+
+    // Config Timer0 Fast PWM mode
+    TCCR2A = (1 << WGM20) | (1 << WGM21) | (1 << COM2B1);
+    TCCR2B = (1 << WGM22);
+
+    // Config pre-scaler
+    TCCR2B |= (1 << CS22); // N=64
+
+    // Config output pin to PWM
+    DDRD = (1 << PD3);
+
+    while (1)
+    {
+    }
+
+    return 0;
 }
