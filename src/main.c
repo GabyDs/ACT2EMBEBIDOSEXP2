b2ca562969d4326faf5712df71a9d18fa65c0eec
Gabriel Da Silva <gabrields@pop-os.localdomain>
2023-10-16 18:02:35 -0300

    config display lcd

--- src/main.c
+++ src/main.c
@@ -4,12 +4,30 @@
 #include <stdio.h>
 #include <avr/interrupt.h>
 
+// -------------------- DISPLAY LCD --------------------
+// Pins of control
+#define RS eS_PORTB0
+#define EN eS_PORTB1
+
+// Pins of data
+#define D4 eS_PORTB2
+#define D5 eS_PORTB3
+#define D6 eS_PORTB4
+#define D7 eS_PORTB5
+
+#include <lcd_328P.h>
+// -------------------- DISPLAY LCD --------------------
+
 #define PWM_OUTPUT PD3
 #define P1 PD2 // On/Off
+#define P2 PD1 // Config mode
+#define P3 PD0 // Velocity
 
 #define BOUNCE_DELAY 6 // ms
 
 volatile uint8_t flag = 0;
+volatile uint8_t config_flag = 1;
+char buffer[16]; // Character array that stores strings (16 = Number of LCD rows)
 
 ISR(INT0_vect)
 {
@@ -22,6 +40,7 @@ ISR(INT0_vect)
 
 int main(void)
 {
+
     // Timer2
     // Config compare TOP and BOTTOM to width pulse
     OCR2A = 125;
@@ -41,19 +60,23 @@ int main(void)
     sei();
 
     // Config inputs pin
-    DDRD &= (1 << P1);
-    PORTD = (1 << P1);
+    DDRD &= ~((1 << P1) | (1 << P2) | (1 << P3));
 
     // Config output pin to PWM
     DDRD |= (1 << PWM_OUTPUT);
 
-    // Config PORTB as output to display
+    // Port to display data
     DDRB = 0xFF;
+
+    Lcd4_Init();
+    Lcd4_Clear();
+
+    sprintf(&buffer[0], "Welcome.");
+    Lcd4_Set_Cursor(1, 0);
+    Lcd4_Write_String(buffer);
 
     while (1)
     {
-        if (flag)
-            PORTB |= (1 << PORTB0);
     }
 
     return 0;
